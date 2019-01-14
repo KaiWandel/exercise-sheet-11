00bc112f1cdefa55f144d06b27b7886ddfbc99e5
Malte Opderbeck <malte.opderbeck@icloud.com>
2019-01-14 14:11:43 +0100

    Update FibonacciRechner.java

--- src/FibonacciRechner.java
+++ src/FibonacciRechner.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 
 public class FibonacciRechner {
-//Malte mach mal
-	//los schneller
-	//Nö!
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Geben Sie eine Zahl ein: ");
+		int n = sc.nextInt();
+		System.out.print(FibonacciCalc(n));
+		sc.close();
+	}
+	private static int FibonacciCalc(int n) {
+		if (n <= 0) 
+			return 0;
+		else if (n == 1)
+			return 1;
+		else 
+			return FibonacciCalc (n-1) + FibonacciCalc (n-2);	
+	}
 }
