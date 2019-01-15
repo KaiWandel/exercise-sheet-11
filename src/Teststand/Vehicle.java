7a1b52f5ce28db81af39b5ceea5cb23e39ec8ef9
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-15 14:17:44 +0100

    Create Vehicle.java

---
+++ src/Teststand/Vehicle.java
@@ -0,0 +1,16 @@
+package Teststand;
+
+public class Vehicle extends Producer {
+	
+	//Name
+	//Hersteller
+	//Farbe
+	//Motor
+	//Verbrauch
+	//CO2Ausstoﬂ
+	//Teststand
+	//Verbrauch Messen
+	//CO2 Ausstoﬂ messen
+	//bekommt drehzahl von teststand
+	
+}
