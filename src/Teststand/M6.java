b3c8fc01c37cdce901420e16d52b0639547dd8c8
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-15 16:46:42 +0100

    Update M6.java

--- src/Teststand/M6.java
+++ src/Teststand/M6.java
@@ -2,8 +2,27 @@ package Teststand;
 
 public class M6 extends Vehicle {
 
-	public M6(String vehicleType, String producerName, String color, String engineType, Integer fuelConsumption) {
-		super(vehicleType, producerName, color, engineType, fuelConsumption);
-		// TODO Auto-generated constructor stub
+	public M6(final String color,final String engineType,final Integer fuelConsumption) {
+		super();
+		this.color =  color;
+		this.engineType = engineType;
+		this.fuelConsumption = fuelConsumption;
+		this.vehicleType = "M6";
+		this.producerName = "SMW";		
+	}
+	public static void main(String[] args) {
+		Vehicle Vehicle1 = new M6( "Blue", "Benzin", 3 );
+		Vehicle Vehicle2 = new Tennis("Red", "Diesel", 4);
+		System.out.print(Vehicle1.getColor());
+		System.out.print(Vehicle1.getEngineType());
+		System.out.print(Vehicle1.getVehicleType());
+		System.out.print(Vehicle1.getProducerName());
+		System.out.print(Vehicle1.getFuelConsumption());
+		System.out.print(Vehicle2.getColor());
+		System.out.print(Vehicle2.getEngineType());
+		System.out.print(Vehicle2.getVehicleType());
+		System.out.print(Vehicle2.getProducerName());
+		System.out.print(Vehicle2.getFuelConsumption());
+		
 	}
 }
