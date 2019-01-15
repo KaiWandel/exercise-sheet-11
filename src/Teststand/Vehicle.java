95b8b864a6963cc07d1f8f59376b4ccc0eabb13a
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-15 16:46:32 +0100

    Update Vehicle.java

--- src/Teststand/Vehicle.java
+++ src/Teststand/Vehicle.java
@@ -1,46 +1,20 @@
 package Teststand;
 
-public class Vehicle extends Producer {
+public abstract class Vehicle extends Producer {
 
-	private String color;
-	private String engineType;
-	private Integer fuelConsumption;
-	
-	public Vehicle(final String vehicleType,final String producerName,final String color,final String engineType,final Integer fuelConsumption) {
-		super();
-		this.color = color;
-		this.engineType = engineType;
-		this.fuelConsumption = fuelConsumption;
-	}
+	protected String color;
+	protected String engineType;
+	protected Integer fuelConsumption;
 	
 	public String getColor() {
 		return color;
-	}
-
-	public void setColor(String color) {
-		this.color = color;
 	}
 	
 	public String getEngineType() {
 		return engineType;
 	}
 
-	public void setEngineType(String engineType) {
-		this.engineType = engineType;
-	}
-
 	public Integer getFuelConsumption() {
 		return fuelConsumption;
 	}
-
-	public void setFuelConsumption(Integer fuelConsumption) {
-		this.fuelConsumption = fuelConsumption;
-	}
-
-	//CO2Ausstoﬂ
-	//Teststand
-	//Verbrauch Messen
-	//CO2 Ausstoﬂ messen
-	//bekommt drehzahl von teststand
-	
 }
