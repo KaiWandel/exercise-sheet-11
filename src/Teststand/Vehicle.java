cd840cd6e3e1557259145afb7aee94f8bf37cc39
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-15 14:55:18 +0100

    Update Vehicle.java

--- src/Teststand/Vehicle.java
+++ src/Teststand/Vehicle.java
@@ -1,10 +1,37 @@
 package Teststand;
 
 public class Vehicle extends Producer {
+
+	private String vehicleType;
+	private String producerName;
+	private String color;
+	private Integer Verbrauch;
+	private String engineType;
+	private Integer fuelconsumption;
 	
-	//Name
-	//Hersteller
-	//Farbe
+	public Vehicle(String vehicleType, String producerName, String color) {
+		super();
+		this.color = color; 
+		this.vehicleType = vehicleType;
+		this.producerName = producerName;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public Integer getVerbrauch() {
+		return Verbrauch;
+	}
+
+	public void setVerbrauch(Integer verbrauch) {
+		Verbrauch = verbrauch;
+	}
+
 	//Motor
 	//Verbrauch
 	//CO2Ausstoﬂ
