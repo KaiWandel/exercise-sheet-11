fa1e71df4973e576ff16a43ccd3fd0749cd9f030
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-15 15:14:20 +0100

    Update Tennis.java

--- src/Teststand/Tennis.java
+++ src/Teststand/Tennis.java
@@ -1,5 +1,9 @@
 package Teststand;
 
-public class Tennis {
+public class Tennis extends Vehicle{
 
+	public Tennis(String vehicleType, String producerName, String color, String engineType, Integer fuelConsumption) {
+		super(vehicleType, producerName, color, engineType, fuelConsumption);
+		// TODO Auto-generated constructor stub
+	}
 }
