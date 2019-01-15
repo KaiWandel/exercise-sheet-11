c5aeda118fba92d34272ace473a5f064736a5c30
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-15 15:14:32 +0100

    Update M6.java

--- src/Teststand/M6.java
+++ src/Teststand/M6.java
@@ -1,5 +1,9 @@
 package Teststand;
 
-public class M6 {
+public class M6 extends Vehicle {
 
+	public M6(String vehicleType, String producerName, String color, String engineType, Integer fuelConsumption) {
+		super(vehicleType, producerName, color, engineType, fuelConsumption);
+		// TODO Auto-generated constructor stub
+	}
 }
