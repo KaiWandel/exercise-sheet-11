dcabb7e664694e7953e76ff859ca6e6e95e56d03
Malte Opderbeck <malte.opderbeck@icloud.com>
2019-01-15 14:50:03 +0100

    Create Engine.java

---
+++ src/Teststand/Engine.java
@@ -0,0 +1,12 @@
+package Teststand;
+
+public class Engine {
+	private String fuel;
+	private Integer efficiency;
+	private Integer rpm;
+	private Integer fuelConsumption;
+	
+	public Engine (String fuel, Integer efficiency, Integer rpm, Integer fuelConsumption) {
+		
+	}
+}
