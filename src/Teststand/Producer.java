a406c1d313c9c1019fd5d834ce94303ddf52d9c1
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-15 16:46:39 +0100

    Update Producer.java

--- src/Teststand/Producer.java
+++ src/Teststand/Producer.java
@@ -2,22 +2,14 @@ package Teststand;
 
 public abstract class Producer {
 	
-	private String vehicleType;
-	private String producerName;
+	protected String vehicleType;
+	protected String producerName;
 	
 	public String getProducerName() {
 		return producerName;
 	}
-	
-	public void setProducerName(String producerName) {
-		this.producerName = producerName;
-	}
-	
+
 	public String getVehicleType() {
 		return vehicleType;
-	}
-	
-	public void setVehicleType(String vehicleType) {
-		this.vehicleType = vehicleType;
 	}
 }
