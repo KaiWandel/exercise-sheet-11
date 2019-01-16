4229e6eaa4ff6008711e72ffb372ffa25332ed2d
Malte Opderbeck <malte.opderbeck@icloud.com>
2019-01-16 10:12:25 +0100

    Update Test.java

--- src/Teststand/Test.java
+++ src/Teststand/Test.java
@@ -3,6 +3,48 @@ package Teststand;
 public class Test extends Teststand {
 	private Integer rpm;
 	private Double efficiency;
-	private Integer fuelConsumption;
-	private Integer 
+	private Double fuelConsumption;
+	private Integer carbonEmission;
+	
+	public Test(Integer rpm, Double efficiency, Double fuelConsumption, Integer carbonEmission) {
+		super();
+		this.rpm = rpm;
+		this.efficiency = efficiency;
+		this.fuelConsumption = fuelConsumption;
+		this.carbonEmission = carbonEmission;
+	}
+
+	public Integer getRpm() {
+		return rpm;
+	}
+
+	public void setRpm(Integer rpm) {
+		this.rpm = rpm;
+	}
+
+	public Double getEfficiency() {
+		return efficiency;
+	}
+
+	public void setEfficiency(Double efficiency) {
+		this.efficiency = efficiency;
+	}
+
+	public Double getFuelConsumption() {
+		return fuelConsumption;
+	}
+
+	public void setFuelConsumption(Double fuelConsumption) {
+		this.fuelConsumption = fuelConsumption;
+	}
+
+	public Integer getCarbonEmission() {
+		return carbonEmission;
+	}
+
+	public void setCarbonEmission(Integer carbonEmission) {
+		this.carbonEmission = carbonEmission;
+	}
+	
+	
 }
