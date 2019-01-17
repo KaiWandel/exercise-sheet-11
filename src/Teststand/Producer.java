9358bf75cf0afbb3574c54696de4a166638e5716
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-17 19:47:22 +0100

    Update Producer.java

--- src/Teststand/Producer.java
+++ src/Teststand/Producer.java
@@ -1,14 +1,24 @@
 package Teststand;
 
+/*
+ * Abstrakte Klasse Producer
+ * Oberklasse von Vehicle
+ * 
+ * KLasse die Getter Methode besitzt und einen Fahrzeug Typ und einen Hersteller namen als String enthält
+ */
 public abstract class Producer {
 	
 	protected String vehicleType;
 	protected String producerName;
-	
+	/*
+	 * getter Methode für den String des Herstellernamen
+	 */
 	public String getProducerName() {
 		return producerName;
 	}
-
+	/*
+	 * getter Methode für den String des Fahrzeug Typen
+	 */
 	public String getVehicleType() {
 		return vehicleType;
 	}
