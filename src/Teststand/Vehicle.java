832419dbb15c04fda55999408ce42c2407031081
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-17 19:46:55 +0100

    Update Vehicle.java

--- src/Teststand/Vehicle.java
+++ src/Teststand/Vehicle.java
@@ -1,20 +1,37 @@
 package Teststand;
-
+/*
+ * Abstrakte Klasse die von der Klasse Producer erbt und die Oberklasse der KLassen Tennis und M6 ist
+ * 
+ * sie besitzt einen String mit der Farbe
+ * einen Enum mit dem Motortypen
+ * einen Double mit dem Treibstoffverbrauch
+ */
 public abstract class Vehicle extends Producer {
-
-	protected String color;
-	protected String engineType;
-	protected Integer fuelConsumption;
 	
+	protected enum EngineType {
+		BENZIN, DIESEL
+	}
+	protected String color;
+	protected Double fuelConsumption;
+	/*
+	 * Getter Methode für den String Farbe 
+	 */
 	public String getColor() {
 		return color;
 	}
+	/*
+	 * Getter Methode für den String Motortyp, kann nur Benzin oder Diesel sein
 	
 	public String getEngineType() {
-		return engineType;
+		for (EngineType.BENZIN)
+		return "Benziner";
 	}
-
-	public Integer getFuelConsumption() {
+	*/ 
+	
+	/*
+	 * Getter Methode für den Double des Treibstoffverbrauchs
+	 */
+	public Double getFuelConsumption() {
 		return fuelConsumption;
 	}
 }
