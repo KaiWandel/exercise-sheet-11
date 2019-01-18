5e0d5f8a53fe41fc6d49f73944b8ed8565acbda7
Malte Opderbeck <malte.opderbeck@icloud.com>
2019-01-18 20:16:59 +0100

    Update Teststand.java

--- src/Teststand/Teststand.java
+++ src/Teststand/Teststand.java
@@ -1,5 +1,10 @@
 package Teststand;
-
+/*
+ * Die Klasse Teststand
+ * 
+ * sie besitzt eine Array-Liste mit eine eine Anzahl von sieben Integer
+ * Die Array-Liste heißt testdrehzahl und beinhaltet sieben verschiedene Drehzahlen
+ */
 public class Teststand {
 	Integer[] TestDrehzahlen = new Integer[] {1000,2000,3000,4000,5000,6000};
 }
