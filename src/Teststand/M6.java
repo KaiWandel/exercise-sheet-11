2c5c3c42f47902807c775d4917bac330a70efa39
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-18 13:21:29 +0100

    Update M6.java

--- src/Teststand/M6.java
+++ src/Teststand/M6.java
@@ -2,23 +2,24 @@ package Teststand;
 
 public class M6 extends Vehicle {
 
-	public M6(final String color,final EngineType engineType,final Double fuelConsumption) {
+	public M6(final String color,final String engineType,final Double fuelConsumption) {
 		super();
 		this.color =  color;
 		this.fuelConsumption = fuelConsumption;
+		this.engineType = engineType;
 		this.vehicleType = "M6";
 		this.producerName = "SMW";		
 	}
 	public static void main(String[] args) {
-		Vehicle Vehicle1 = new M6( "Blue", EngineType.BENZIN, 3.4 );
-		Vehicle Vehicle2 = new Tennis("Red", EngineType.DIESEL , 4.7);
+		Vehicle Vehicle1 = new M6( "Blue", "BENZIN" , 3.4 );
+		Vehicle Vehicle2 = new Tennis("Red", "DIESEL" , 4.7);
 		System.out.print(Vehicle1.getColor());
-		//System.out.print(Vehicle1.getEngineType());
+		System.out.print(Vehicle1.getEngineType());
 		System.out.print(Vehicle1.getVehicleType());
 		System.out.print(Vehicle1.getProducerName());
 		System.out.print(Vehicle1.getFuelConsumption());
 		System.out.print(Vehicle2.getColor());
-		//System.out.print(Vehicle2.getEngineType());
+		System.out.print(Vehicle2.getEngineType());
 		System.out.print(Vehicle2.getVehicleType());
 		System.out.print(Vehicle2.getProducerName());
 		System.out.print(Vehicle2.getFuelConsumption());
