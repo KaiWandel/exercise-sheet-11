67dff6b756b96a340690840f8d3b0997c35b1866
Malte Opderbeck <malte.opderbeck@icloud.com>
2019-01-18 20:17:10 +0100

    Update mainMethode.java

--- src/Teststand/mainMethode.java
+++ src/Teststand/mainMethode.java
@@ -3,14 +3,6 @@ package Teststand;
 
 public class mainMethode {
 	public static void main(String[] args) {
-		// Testfahrzeug erstellen
-		
-		// Motor erzeugen
-		// Fahrzeug einen Test zuordnen
-		// Motor über Fahrzeug die Inputdaten des Teststands empfangen lassen
-		// Motor Inoutdaten verarbeiten lassen und CO2 Wert an das Fahrzeug weiterleiten
-		// Fahrzeug übermittelt CO2 Wert an teststand
-		// neues Fahrzeug oder Inputdaten erstellen
 		Vehicle Vehicle1 = new M6( "Blue", "BENZIN" , 3.4 );
 		Engine Engine1 = new Engine("BENZIN" , 0.8 , 0, 0.0, 0.0 );
 	}
