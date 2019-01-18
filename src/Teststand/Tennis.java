233a8a1dff172bafe8c5b5d4f54450c9cb67609c
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-18 13:21:27 +0100

    Update Tennis.java

--- src/Teststand/Tennis.java
+++ src/Teststand/Tennis.java
@@ -2,12 +2,13 @@ package Teststand;
 
 public class Tennis extends Vehicle{
 
-	public Tennis(final String color,final EngineType enginType ,final Double fuelConsumption) {
+	public Tennis(final String color,final String engineType ,final Double fuelConsumption) {
 		super();
 		this.color =  color;
 		this.fuelConsumption = fuelConsumption;
+		this.engineType = engineType;
 		this.vehicleType = "Tennis";
 		this.producerName = "IW";
-		this.EngineType = EngineType
+
 	}
 }
