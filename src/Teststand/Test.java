8cc7dc719786f00ce75c5d5c41f333f159f8fe44
KaiWandel <44360057+KaiWandel@users.noreply.github.com>
2019-01-18 14:49:01 +0100

    Update Test.java

--- src/Teststand/Test.java
+++ src/Teststand/Test.java
@@ -1,26 +1,23 @@
 package Teststand;
 
 public class Test extends Teststand {
-	private Integer rpm;
-	private String testName;
-	Integer[] TestDrehzahlen = new Integer[] {1000,2000,3000,4000,5000,6000};
-	public Test(Integer rpm, String testName) {
-		super();
-		this.rpm = rpm;
-		this.testName = testName;
-	}
-
-
-	public Integer getRpm() {
-		return rpm;
-	}
-
-	public Double CO2Test() {
+	
+	public static void main(String[] args) {
+		// Testfahrzeug erstellen
 		
+		// Motor erzeugen
+		// Fahrzeug einen Test zuordnen
+		// Motor über Fahrzeug die Inputdaten des Teststands empfangen lassen
+		// Motor Inoutdaten verarbeiten lassen und CO2 Wert an das Fahrzeug weiterleiten
+		// Fahrzeug übermittelt CO2 Wert an teststand
+		// neues Fahrzeug oder Inputdaten erstellen
+		Vehicle Vehicle1 = new M6( "Blue", "BENZIN" , 3.4 );
+		Engine Engine1 = new Engine("BENZIN" , 0.8 , 0, 0.0, 0.0 );
 	}
-}
-
-public static void main (String[] args) {
-	Vehicle Vehicle1 = new M6( "Blue", "BENZIN" , 3.4 );
-	Engine Engine1 = new Engine("BENZIN" , 0.8 , 0, 0.0, 0.0 );
+	public Double CO2Test() {
+		for (Integer rpm : TestDrehzahlen) {
+			double Ergebnis = Engine.carbonEmissionsCalc()
+			return Ergebnis;
+		}
+	}
 }
